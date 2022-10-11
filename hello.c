81ec1d27dff076767e1ef404551fe11f8f31ea7e
Diya Roongta <diyaroongta@DRs-MacBook-Pro.local>
2022-10-11 18:01:57 +0530

    I shifted hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+// #include <stdio.h> 
+
+// int main(int argc, char **argv) {
+//     int x=5;
+//     x=6;
+//     printf("%d\n", x);
+//     return 0;
+// }
+
+#include <stdio.h>
+int main(int argc, char **argy) {
+int x = 0;
+int y = x++;
+
+printf("x = %d, y = %d\n", x, y);
+}
