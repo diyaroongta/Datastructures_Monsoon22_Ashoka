08d6b7e08637ba549527f1056ac41932bc42993a
dro91 <114307190+dro91@users.noreply.github.com>
2022-10-23 16:45:47 +0530

    Update stresstest.c

--- stresstest.c
+++ stresstest.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// TASK: I want you to change the increment and n and see at what point you end up with an extra loop
-
 int main(int argc, char** argv){
 
     float i;
-    float increment = 0.000000001; //Change around this
-    float n = 0.00000001; //And this (this is 10 times increment)
+    float increment = 0.000000000001; //At this value, when we try to increment it upto n, it should be 100 times increment but the system gives 101 increments
+    float n = 0.0000000001; 
     int count = 1;
 
     for (i=0.0; i<n; i+=increment) {
         printf("Loop Number: %d\n", count);
+        printf("Value: %f\n", i);
         count++;
     }
 
     return 0;
 }
+
