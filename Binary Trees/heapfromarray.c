226e87c19f9910a1d859ec8c88233bd39531afd4
dro91 <114307190+dro91@users.noreply.github.com>
2022-12-09 02:16:35 +0530

    Update heapfromarray.c

--- Binary Trees/heapfromarray.c
+++ Binary Trees/heapfromarray.c
@@ -15,7 +15,7 @@ void swap(int* p, int* q) //function to swap two inputed integers
 //heapify converts an array into a heap
 void heapify(int array[], int n, int k) //creating heap from an array
 {
-    //we will compare the three elemenets — left node, right node and temp_large 
+    //we will compare the three elemenets — left node, right node and temp_large to see which one is larger
     int temp_large = k; 
     int left = 2 * k + 1;  
     int right = 2 * k + 2;
@@ -53,23 +53,18 @@ void sorting(int array[], int n)//function to execute heap sort (maxheap) on the
 void heap_sort(int array[], int n)
 {
     for (int j = n / 2; j >= 0; j--)// to build a max heap
-    
-    // j can not be negative because it is the position of an element in the array  
-    // j will start from the middle position of the array 
-    // decrement to reach the 0th posiiton 
-        heapify(array, n, j);// carry out heapify function for the loop mentioned above 
+        //j goes from middle to 0th position of heap
+        heapify(array, n, j);//
 
-    for (int i = n - 1; i >= 0; i--) { // for heap sort 
-    // i can not be negative because it is the position of an element in the array  
-        // decrement to reach the 0th posiiton 
-    swap(&array[0], &array[i]); 
+    for (int i = n - 1; i >= 0; i--) { 
+        swap(&array[0], &array[i]); 
     }
 }
       
 int main()
 {
-    int array[] = { 123, 1145, 1, 50, 26, 97, 77 }; // random array 
-    int n = sizeof(array) / sizeof(array[0]); // to calculate the number of elements in the array 
+    int array[] = { 13, 27, 41, 22, 86, 56, 94}; //sample array 
+    int n = sizeof(array) / sizeof(array[0]); //
     // dividing total space allocation by space allocation of one element 
     heap_sort(array, n);
     printf("Minimum Element of Max Heap:");
