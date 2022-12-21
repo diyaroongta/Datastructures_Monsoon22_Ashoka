5af79b5a6e0563ab3f66a4246301b7d96cee3ad4
dro91 <114307190+dro91@users.noreply.github.com>
2022-12-21 23:02:15 +0530

    Update AVLminmax.c

--- AVLminmax.c
+++ AVLminmax.c
@@ -1,18 +1,1 @@
-//Finding the minimum node of the tree
 
-NodeAddress min (NodeAddress root)
-{
-    NodeAddress x = root;
-    while (x -> left)
-        x = x -> left;
-    return x;
-}
-
-//AVL tree maximum
-NodeAddress maximum (NodeAddress root)
-{
-    NodeAddress x = root;
-    while (x -> right)
-        x = x -> right;
-    return x;
-}
