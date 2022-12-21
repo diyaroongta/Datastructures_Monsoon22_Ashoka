048e3ec2c6fe03f75b5235c317b51cc9b2915d9b
dro91 <114307190+dro91@users.noreply.github.com>
2022-12-21 23:11:06 +0530

    Update bubble sort.c

--- Arrays/bubble sort.c
+++ Arrays/bubble sort.c
@@ -1,1 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+
+void bubblesort(int *arr, int n)
+  
+  int i, j, temp; //number of elements already sorted
+
+    for(i = 0; i<n; i++){  
+      for(j=0; j<n-1-i; j++){
+            if(arr[j]>arr[j+1]){
+                //swap elements if next element is smaller than the previous
+                temp= arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }   //end of inner for loop
+
+    }  //end of outer for loop
+}
+
+
+//function to generate an array of size t with random integers
+int * generateArray(int n) {
+	int * t = malloc( n * sizeof(int) );
+	if(t) {
+		for(int i=0; i<n; i++) {t[i] = rand()%1000;}
+	}
+	return t;
+}
+
+//function to print the array 
+void printArray(int * a, int n) {
+	printf("Array = ");
+	for(int i=0; i<n; i++) {
+		printf( i?", %d":"%d", a[i]);
+	}
+	printf(".\n");
+}
+
+//main method - to call the functions above 
+int main(int argc, char **argv) {
+	int * a;
+	int n = 10;
+
+	srand(time(NULL));
+
+	a = generateArray(n);
+    printArray(a,n);
+
+    bubbleSort(a,n);
+
+    printf("Sorted array: \n");
+    printArray(a, n);
+    return 0;
+}
